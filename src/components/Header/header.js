39724d17459e8ea200bbb18d150256e38d604b1b
llcain <llcain2012@gmail.com>
2023-03-09 18:25:08 -0600

    created header and main components

---
+++ src/components/Header/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+const Header = () => (
+  <div className="header">
+    <h1>LC</h1>
+    <ul>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#news">About</a></li>
+      <li><a href="#contact">Portfolio</a></li>
+      <li><a href="#about">Skills</a></li>
+      <li><a href="#about">Contact</a></li>
+    </ul>
+  </div>
+);
+export default Header;

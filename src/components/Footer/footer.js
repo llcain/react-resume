42cf7eb7651714756ef8d7a01503fdb46f292a4b
llcain <llcain2012@gmail.com>
2023-03-09 22:48:03 -0600

    update footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 
 import "./Footer.css";
@@ -8,7 +8,7 @@ const Footer = () => (
   <div className="footer">
     <h1>Footer</h1>
     <Button variant="contained">Hello World</Button>
-    <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+    
     
 
   </div>
